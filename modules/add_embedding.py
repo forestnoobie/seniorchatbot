becaa735eddc0ee8ba3c61329f1410a72f267453
forestnoobie <nakyilkim@naver.com>
2024-04-24 20:56:16 +0900

    text embedding

--- modules/add_embedding.py
+++ modules/add_embedding.py
@@ -8,11 +8,27 @@ from dataclasses import dataclass
 from google.cloud import aiplatform, storage
 from google.protobuf import struct_pb2
 import numpy as np
-import tempfile
 
+import vertexai
+from vertexai.language_models import TextEmbeddingModel
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_vertexai import (
+    VectorSearchVectorStore,
+    VectorSearchVectorStoreDatastore,
+)
+
+from google.cloud.aiplatform.matching_engine.matching_engine_index_endpoint import (
+    Namespace,
+    NumericNamespace,
+)
+
+import tempfile
 from absl import app
 from absl import flags
 import streamlit as st
+
+
 
 # _IMAGE_FILE = flags.DEFINE_string('image_file', None, 'Image filename')
 # _TEXT = flags.DEFINE_string('text', None, 'Text to input')
@@ -21,6 +37,7 @@ import streamlit as st
 _PROJECT="primeval-argon-420311"
 _INDEX_ENDPOINT="projects/854115243710/locations/us-central1/indexEndpoints/7122253694686461952"
 _DEPLOYED_INDEX_ID="multimodal_embedding_endpoint"
+
 
 
 def show_upload(state:bool):
@@ -87,12 +104,12 @@ class SingletonInstance:
         return cls.__instance
 
 
-class EmbeddingResponse(typing.NamedTuple):
+class ImageEmbeddingResponse(typing.NamedTuple):
     text_embedding: typing.Sequence[float]
     image_embedding: typing.Sequence[float]
 
 
-class StorageClient(SingletonInstance):
+class StorageClient():
     def __init__(self, project: str, bucket: str = "smishing-image"):
         self.client = storage.Client(project = project)
 
@@ -100,26 +117,90 @@ class StorageClient(SingletonInstance):
         bucket = self.client.bucket(bucket = bucket)
         return bucket
         
-    
-    
 
-class VectorSearchClient(SingletonInstance):
-    def __init__(self, index_endpoint: str, deployed_index_id: str):
+class VectorSearchClient():
+    def __init__(self, index_endpoint: str, deployed_index_id: str, bucket_uri: str, index_id: str, endpoint_id: str):
         self.index_endpoint = index_endpoint
         self.deployed_index_id = deployed_index_id
-        self.index = aiplatform.MatchingEngineIndexEndpoint(index_endpoint)
+        self.index_endpoint = aiplatform.MatchingEngineIndexEndpoint(index_endpoint)
+        self.bucket_uri = bucket_uri
+        self.index_id = index_id
+        self.endpoint_id = endpoint_id
+        
 
-    def find_neighbors(self, query, num_neighbors: int = 3):
-        response = self.index.find_neighbors(
+    # similarity search by vector 
+    def find_neighbors(self, query: typing.List[float], num_neighbors: int = 3):
+        response = self.index_endpoint.find_neighbors(
                         deployed_index_id = self.deployed_index_id,
                         queries = [query],
                         num_neighbors = num_neighbors
-                )
+        )
         return response
     
+    def update_index(self):
+        index = aiplatform.MatchingEngineIndex("5709952999040745472")
+        index.update_embeddings(contents_delta_uri=self.bucket_uri, is_complete_overwrite=True)
+
+# Using Google API
+class TextEmbeddingPredictionClient(SingletonInstance):
+    def __init__(self, project = "primeval-argon-420311", location: str = "us-central1", model: str = "textembedding-gecko@001"):
+        self.location = location
+        self.project = project
+        self.model = model
+        
+        vertexai.init(
+		project=self.project,
+		location=self.location
+		)
+        
+        self.client = TextEmbeddingModel.from_pretrained(self.model)
+        
+    def generate_text_embeddings(self, texts: list) -> list:
+        response = self.client.get_embeddings(texts)
+        text_embeddings = [embedding.values for embedding in response]
+        return text_embeddings
+        
+
+# Using Langchain
+class TextEmbeddingClient(SingletonInstance):
+    def __init__(self,  index_id: str, endpoint_id:str, project = "primeval-argon-420311", location: str = "us-central1", model: str = "textembedding-gecko@001", bucket: str = "embedding-text"):
+        self.location = location
+        self.project = project
+        self.embedding_model = VertexAIEmbeddings(model_name=model)
+        self.vectorstore = VectorSearchVectorStore.from_components(
+                            project_id=self.project,
+                            region=self.location,
+                            gcs_bucket_name=bucket,
+                            index_id=index_id,
+                            endpoint_id=endpoint_id,
+                            embedding=self.embedding_model,
+                            stream_update=True,
+                        )
+    # upload text embedding to vectorstore
+    def upload_embedding(self, record_data:typing.List[typing.Dict]):
+        texts = []
+        metadatas = []
+        for record in record_data:
+            record = record.copy()
+            page_content = record.pop("description")
+            texts.append(page_content)
+            if isinstance(page_content, str):
+                metadata = {**record}
+                metadatas.append(metadata)
+                
+        return self.vectorstore.add_texts(texts=texts, metadatas=metadatas, is_complete_overwrite=True)
+
+    # search by text
+    def similarity_search(self, query: str, k: int = 3, filters: dict=None):
+        # Try running a similarity search with text filter
+        # filters = [Namespace(name="season", allow_tokens=["spring"])]
+        return self.vectorstore.similarity_search(query, k=k, filter = filters)
+    
+
+    
             
-class EmbeddingPredictionClient(SingletonInstance):
-    def __init__(self, project: str,
+class ImageEmbeddingPredictionClient(SingletonInstance):
+    def __init__(self, project: str= "primeval-argon-420311",
                  location: str = "us-central1",
                  api_regional_endpoint: str = "us-central1-aiplatform.googleapis.com"):
         
@@ -127,13 +208,14 @@ class EmbeddingPredictionClient(SingletonInstance):
         self.client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
         self.location = location
         self.project = project
-
-    def get_embedding(self, text: str = None, image_bytes: bytes = None, video_uri: str = None,
-                      start_offset_sec: int = 0, end_offset_sec: int = 120, interval_sec: int = 16):
-        if not text and not image_bytes and not video_uri:
-            raise ValueError('At least one of text or image_bytes or video_uri must be specified.')
+        
+    # get image embedding
+    def generate_image_embedding(self, text: str = None, image_bytes: bytes = None):
+        if not text and not image_bytes:
+            raise ValueError('At least one of text or image_bytes must be specified.')
 
         instance = struct_pb2.Struct()
+        
         if text:
             instance.fields['text'].string_value = text
 
@@ -145,6 +227,7 @@ class EmbeddingPredictionClient(SingletonInstance):
         instances = [instance]
         endpoint = (f"projects/{self.project}/locations/{self.location}"
                     "/publishers/google/models/multimodalembedding@001")
+        
         response = self.client.predict(endpoint=endpoint, instances=instances)
 
         text_embedding = None
@@ -157,39 +240,48 @@ class EmbeddingPredictionClient(SingletonInstance):
             image_emb_value = response.predictions[0]['imageEmbedding']
             image_embedding = [v for v in image_emb_value]
          
-        return EmbeddingResponse(
+        return ImageEmbeddingResponse(
             text_embedding=text_embedding,
             image_embedding=image_embedding)
 
 
-class EmbeddingClient:
+class ImageEmbeddingClient(SingletonInstance):
     def __init__(self, project: str = "primeval-argon-420311", location: str = "us-central1"):
-        self.storage_client = storage.Client()
         self.location = location
         self.project = project
-        self.client = EmbeddingPredictionClient(project = self.project)
+        self.storage_client = storage.Client(project = self.project)
+        self.client = ImageEmbeddingPredictionClient(project = self.project)
         
 
     def list_blobs(self, bucket_name):
         """Lists all the blobs in the bucket."""
-        blobs = storage_client.list_blobs(bucket_name)
+        blobs = self.storage_client.list_blobs(bucket_name)
         return [blob.name for blob in blobs]
 
+    # download images from bucket
     def download_images(self, bucket_name):
-        storage_client = storage.Client()
-        bucket = storage_client.bucket(bucket_name)
-        image_paths = [ obj_nm for obj_nm in list_blobs(bucket_name)]
+        bucket = self.storage_client.bucket(bucket_name)
+        image_paths = [ obj_nm for obj_nm in self.list_blobs(bucket_name)]
 
         return [bucket.blob(image).download_as_bytes() for image in image_paths]
     
+<<<<<<< HEAD
     def make_embedding(self, bucket_name):
         images_byte = self.download_images(bucket_name)       
         #client to access multimodal-embeddings model to convert text to embeddings
         images_embedding = [self.e.get_embedding(image_bytes=image) for image in images_byte]
+=======
+    # make embeddings from bucket
+    def make_embeddings(self, bucket_name):
+        images_bytes = self.download_images(bucket_name)       
+        #client to access multimodal-embeddings model to convert image to embeddings
+        images_embeddings = [self.client.generate_image_embedding(image_bytes=image) for image in images_bytes]
+>>>>>>> 1794cbf (adding text embedding)
 
-        return images_embedding
-    
-    def upload_embedding(self,  images_path, images_embedding, embedding_url: str = "gs://embedding-image/", 
+        return images_embeddings
+        
+    # upload embeddings to bucket
+    def upload_embedding(self, images_path, images_embedding, embedding_url: str = "gs://embedding-image/", 
                         ):
         # Create temporary file to write embeddings to
         embeddings_file = tempfile.NamedTemporaryFile(suffix=".json", delete=False)
@@ -209,6 +301,7 @@ class EmbeddingClient:
             f.writelines(embeddings_formatted)
             
         call(["gsutil", "cp", f"{embeddings_file.name}", f"{embedding_url}"])
+
      
 
 
